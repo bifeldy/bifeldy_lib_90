068f67fae4ac794bae80b616054ffaf84918e120
B. Bias A. Ch <bias@indomaret.co.id>
2025-12-13 12:48:24 +0700

    Add project files.

---
+++ bifeldy_lib_90/Bifeldy.cs
@@ -0,0 +1,7 @@
+﻿namespace bifeldy_lib_90 {
+
+    public static class Bifeldy {
+        //
+    }
+
+}
---
+++ bifeldy_lib_90/Extensions/JsonSerializationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Serialization.Metadata;
+
+namespace bifeldy_lib_90.Extensions {
+
+    public static class JsonSerializationExtensions {
+
+        public static IJsonTypeInfoResolver[] JsonTypeInfoResolvers = {
+            // TODO: Add additional JsonSerializerContext generated resolvers here
+        };
+
+        public static IServiceCollection ConfigureHttpJsonOptionsEx(this IServiceCollection services, IJsonTypeInfoResolver[] jsonTypeInfoResolversExtended) {
+            return services.ConfigureHttpJsonOptions(options => {
+                options.SerializerOptions.PropertyNamingPolicy = null;
+
+                foreach (IJsonTypeInfoResolver resolver in JsonTypeInfoResolvers.Concat(jsonTypeInfoResolversExtended)) {
+                    options.SerializerOptions.TypeInfoResolverChain.Add(resolver);
+                }
+            });
+        }
+
+    }
+
+}
