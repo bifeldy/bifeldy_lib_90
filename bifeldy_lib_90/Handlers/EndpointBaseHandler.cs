4abdc66e38d601ac479cb10dce6bb29dd4c032c2
B. Bias A. Ch <bias@indomaret.co.id>
2026-01-06 09:15:35 +0700

    Base Handler ~

---
+++ bifeldy_lib_90/Handlers/EndpointBaseHandler.cs
@@ -0,0 +1,164 @@
+﻿using bifeldy_lib_90.Abstractions;
+using bifeldy_lib_90.Attributes;
+using bifeldy_lib_90.Backgrounds;
+using bifeldy_lib_90.Models;
+using bifeldy_lib_90.Repositories;
+using bifeldy_lib_90.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization.Metadata;
+
+namespace bifeldy_lib_90.Handlers {
+
+    public interface IEndpointBaseHandler {
+        CEndpointBaseHandler SetHttpContext(HttpContext context);
+        CEndpointBaseHandler SetService(IServiceBaseHandler baseService);
+        CEndpointBaseHandler Initialize(HttpContext context, IServiceBaseHandler baseService);
+        IResult GetTableClassStructureModel<T>(JsonTypeInfo<T> jsonTypeInfo, [CallerMemberName] string callerMemberName = null);
+        IResult GetPocoStructureModel<T>(JsonTypeInfo<T> jsonTypeInfo, [CallerMemberName] string callerMemberName = null);
+    }
+
+    public abstract class CEndpointBaseHandler : IEndpointBaseHandler {
+
+        protected HttpContext _context;
+
+        protected readonly CronScheduler _scheduler;
+        //
+        protected readonly ILogger<CEndpointBaseHandler> _logger;
+        protected readonly IServiceProvider _sp;
+        protected readonly IGlobalService _gs;
+        protected readonly IApplicationService _app;
+        protected readonly IHttpService _http;
+        protected readonly IConverterService _cs;
+        protected readonly IRdlcService _rs;
+        protected readonly IGeneralRepository _generalRepo;
+
+        //
+        // Ini Nantinya Akan Ketimpa Sama Class Turunannya
+        //
+        protected IServiceBaseHandler _baseService;
+
+        public CEndpointBaseHandler(
+            CronScheduler scheduler,
+            //
+            ILogger<CEndpointBaseHandler> logger,
+            IServiceProvider sp,
+            IGlobalService gs,
+            IApplicationService app,
+            IHttpService http,
+            IConverterService cs,
+            IRdlcService rs,
+            IGeneralRepository generalRepo
+        ) {
+            this._scheduler = scheduler;
+            //
+            this._logger = logger;
+            this._sp = sp;
+            this._gs = gs;
+            this._app = app;
+            this._http = http;
+            this._cs = cs;
+            this._rs = rs;
+            this._generalRepo = generalRepo;
+        }
+
+        public CEndpointBaseHandler SetHttpContext(HttpContext context) {
+            this._context = context;
+            return this;
+        }
+
+        public CEndpointBaseHandler SetService(IServiceBaseHandler baseService) {
+            this._baseService = baseService;
+            return this;
+        }
+
+        public CEndpointBaseHandler Initialize(HttpContext context, IServiceBaseHandler baseService) {
+            return this.SetHttpContext(context).SetService(baseService);
+        }
+
+        protected async Task<IResult> CheckExcludeJenisDc(IDatabase db, string kodeDc, [CallerMemberName] string callerMemberName = null) {
+            string targetKodeDc = await this._generalRepo.GetKodeDc(db);
+            EJenisDc targetJenisDc = await this._generalRepo.GetJenisDc(db);
+
+            if (string.IsNullOrEmpty(kodeDc) && targetJenisDc != EJenisDc.HO && targetJenisDc != EJenisDc.NONDC) {
+                return Results.BadRequest(new ResponseJsonSingle<ResponseJsonMessage>() {
+                    info = $"400 - {callerMemberName}",
+                    result = new ResponseJsonMessage() {
+                        message = "Data Tidak Lengkap!"
+                    }
+                });
+            }
+
+            string jenisDc = targetJenisDc.ToString();
+            if (kodeDc != targetKodeDc) {
+                jenisDc = await this._generalRepo.GetJenisDc(db, kodeDc);
+            }
+
+            if (Enum.TryParse(jenisDc, true, out EJenisDc eJenisDc)) {
+                targetKodeDc = kodeDc;
+                targetJenisDc = eJenisDc;
+            }
+            else {
+                return Results.NotFound(new ResponseJsonSingle<ResponseJsonMessage>() {
+                    info = $"404 - {callerMemberName}",
+                    result = new ResponseJsonMessage() {
+                        message = $"Kode DC {kodeDc.ToUpper()} Tidak Tersedia"
+                    }
+                });
+            }
+
+            Endpoint endpoint = this._context.GetEndpoint();
+            IEnumerable<object> attribs = endpoint?.Metadata
+                .Where(t => typeof(DenyAccessAttribute).IsAssignableFrom(t.GetType()));
+
+            bool isAllowed = true;
+            foreach (DenyAccessAttribute attrib in attribs.Cast<DenyAccessAttribute>()) {
+                isAllowed = this._gs.IsAllowedRoutingTarget(attrib.GetType(), targetKodeDc, targetJenisDc);
+
+                if (!isAllowed) {
+                    break;
+                }
+            }
+
+            if (!isAllowed) {
+                return Results.Json(
+                    new ResponseJsonSingle<ResponseJsonMessage>() {
+                        info = $"403 - {callerMemberName}",
+                        result = new ResponseJsonMessage() {
+                            message = $"Tidak Dapat Menggunakan DC :: `{targetJenisDc}` :: Karena Masuk Dalam Daftar Pengecualian"
+                        }
+                    },
+                    ResponseJsonSerializerContext.Default.ResponseJsonSingleResponseJsonMessage,
+                    MediaTypeNames.Application.Json,
+                    StatusCodes.Status403Forbidden
+                );
+            }
+
+            return null;
+        }
+
+        public IResult GetTableClassStructureModel<T>(JsonTypeInfo<T> jsonTypeInfo, [CallerMemberName] string callerMemberName = null) {
+            return Results.Ok(new ResponseJsonSingle<CTableClassModel>() {
+                info = $"200 - {callerMemberName}",
+                result = new CTableClassModel() {
+                    table_name = typeof(T).Name,
+                    properties = this._cs.GetTableClassStructureModel<T>(jsonTypeInfo)
+                }
+            });
+        }
+
+        public IResult GetPocoStructureModel<T>(JsonTypeInfo<T> jsonTypeInfo, [CallerMemberName] string callerMemberName = null) {
+            return Results.Ok(new ResponseJsonSingle<CPocoModel>() {
+                info = $"200 - {callerMemberName}",
+                result = new CPocoModel() {
+                    poco_name = typeof(T).Name,
+                    properties = this._cs.GetPocoStructureModel<T>(jsonTypeInfo)
+                }
+            });
+        }
+
+    }
+
+}
---
+++ bifeldy_lib_90/Handlers/ServiceBaseHandler.cs
@@ -0,0 +1,171 @@
+﻿using bifeldy_lib_90.Abstractions;
+using bifeldy_lib_90.Repositories;
+using bifeldy_lib_90.Services;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace bifeldy_lib_90.Handlers {
+
+    public interface IServiceBaseHandler { }
+
+    public abstract class CServiceBaseHandler : IServiceBaseHandler {
+
+        protected readonly ILogger<CServiceBaseHandler> _logger;
+        protected readonly IServiceProvider _sp;
+        protected readonly IConverterService _cs;
+        protected readonly IGeneralRepository _generalRepo;
+
+        protected readonly ulong MAX_ROW_PER_PAGE = 500;
+        protected readonly ulong MAX_CHUNK_SIZE = 2048;
+
+        private string logTfApiName = null;
+        protected string LogTfApiName {
+            get => !string.IsNullOrEmpty(this.logTfApiName) ? this.logTfApiName : this.GetType().Name;
+            set => this.logTfApiName = value;
+        }
+
+        public CServiceBaseHandler(
+            ILogger<CServiceBaseHandler> logger,
+            IServiceProvider sp,
+            IConverterService cs,
+            IGeneralRepository generalRepo
+        ) {
+            this._logger = logger;
+            this._sp = sp;
+            this._cs = cs;
+            this._generalRepo = generalRepo;
+        }
+
+        protected async Task CatatLogTfApi<TFormDataStream>(HttpContext http, IDatabase db, object formDataStream, JsonTypeInfo<TFormDataStream> jsonTypeInfo = null, string statusErrMsg = null, DateTime? dateTimeStart = null) {
+            if (dateTimeStart == null) {
+                dateTimeStart = (DateTime)http.Request.HttpContext.Items["request_start_at"];
+            }
+
+            string typeFile = $"{this.logTfApiName} - {http.Request.Method}";
+            if (typeFile.Length > 45) {
+                typeFile = typeFile[..45];
+            }
+
+            string ipOrigin = http.Items["address_ip_proxy"].ToString();
+
+            string data_json = null;
+            if (formDataStream != null) {
+                Type type = formDataStream.GetType();
+
+                if (typeof(Stream).IsAssignableFrom(type)) {
+                    data_json = "[_01_DATA_STREAM_10_]";
+                }
+                else {
+                    try {
+                        data_json = this._cs.ObjectToJson((TFormDataStream)formDataStream, jsonTypeInfo);
+                    }
+                    catch {
+                        data_json = "[_|_UNKNOWN_|_]";
+                    }
+                }
+            }
+
+            string hostPort = http.Request.Host.Host;
+            int? port = http.Request.Host.Port;
+            if (port != null) {
+                hostPort += $":{port}";
+            }
+
+            string requestPath = http.Request.Path.Value;
+            if (http.Request.Headers.TryGetValue(Bifeldy.NGINX_PATH_NAME, out StringValues pathBase)) {
+                string proxyPath = pathBase.Last();
+                if (!string.IsNullOrEmpty(proxyPath)) {
+                    if (!requestPath.StartsWith(proxyPath)) {
+                        requestPath = $"{proxyPath}{requestPath}";
+                    }
+                }
+            }
+
+            string url_destination = $"http://{hostPort}{requestPath}";
+
+            // Max 100
+            if (url_destination.Length > 95) {
+                url_destination = url_destination[..95];
+                url_destination += "...";
+            }
+
+            // Max 500
+            string token = http.Items["token"].ToString();
+            if (token.Length > 495) {
+                token = token[..495];
+                token += "...";
+            }
+
+            // Max 1000
+            if (statusErrMsg?.Length > 995) {
+                statusErrMsg = statusErrMsg[..995];
+                statusErrMsg += "...";
+            }
+
+            string sqlQuery = $@"
+                INSERT INTO dc_logtf_api_t (kode_dc, tgl_request, ip_client, type_file, status, start_process, end_process, data_json, url_destination, token, keterangan)
+                VALUES (:kode_dc, :tgl_request, :ip_client, :type_file, :status, :start_process, :end_process, :data_json, :url_destination, :token, :keterangan)
+            ";
+
+            var sqlParam = new DynamicParameters();
+            sqlParam.Add("kode_dc", await this._generalRepo.GetKodeDc(db));
+            sqlParam.Add("tgl_request", dateTimeStart?.Date);
+            sqlParam.Add("ip_client", ipOrigin);
+            sqlParam.Add("type_file", typeFile);
+            sqlParam.Add("status", string.IsNullOrEmpty(statusErrMsg) ? "SUKSES" : "ERROR");
+            sqlParam.Add("start_process", dateTimeStart);
+            sqlParam.Add("end_process", DateTime.Now);
+            sqlParam.Add("data_json", data_json);
+            sqlParam.Add("url_destination", url_destination);
+            sqlParam.Add("token", token);
+            sqlParam.Add("keterangan", statusErrMsg);
+
+            _ = await db.ExecQueryAsync(sqlQuery, sqlParam);
+        }
+
+        protected async IAsyncEnumerable<T> GetRequestJsonStreamData<T>(HttpContext http, JsonTypeInfo<T> jsonTypeInfo) {
+            string contentType = http.Request.ContentType;
+
+            if (string.Equals(contentType, "application/json", StringComparison.OrdinalIgnoreCase)) {
+                await foreach (T item in JsonSerializer.DeserializeAsyncEnumerable(http.Request.Body, jsonTypeInfo, http.RequestAborted)) {
+                    if (item == null) {
+                        continue;
+                    }
+
+                    yield return item;
+                }
+            }
+            else if (string.Equals(contentType, "application/x-ndjson", StringComparison.OrdinalIgnoreCase)) {
+                using (var reader = new StreamReader(http.Request.Body)) {
+                    string line = null;
+
+                    while ((line = await reader.ReadLineAsync(http.RequestAborted)) != null) {
+                        T item = default;
+
+                        try {
+                            item = JsonSerializer.Deserialize(line, jsonTypeInfo);
+                        }
+                        catch {
+                            throw new Exception("Format X-(ND)JSON Harus Per Baris 1 Object Lengkap");
+                        }
+
+                        if (item == null) {
+                            continue;
+                        }
+
+                        yield return item;
+                    }
+                }
+            }
+            else {
+                throw new Exception($"Streaming Untuk Content-Type '{contentType}' Tidak Tersedia");
+            }
+        }
+
+    }
+
+}
